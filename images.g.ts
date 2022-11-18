2e83621aba2820be0ba2bb9f3707db945c83772a
cilander86 <118524938+cilander86@users.noreply.github.com>
2022-11-18 18:00:35 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "trap monster":return img`
+. . . . . a a a a a . . . . . . 
+. . . . . . a a a . . . . . . . 
+. . . . . . . a . . . . . . . . 
+. . . . . . . a . . . . . . . . 
+. . . . . . . a . . . . . . . . 
+. 1 f 1 . . . a . . . 1 1 f . . 
+. 1 1 1 . . . a . . . 1 1 1 . . 
+. . f . . . . a . . . . f . . . 
+. f f f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . . 
+. f 1 b 1 1 b 1 1 1 b b 1 f . . 
+. f b 1 b b 1 b b b 1 1 1 f . . 
+. f f f f f f 1 b b f f f f . . 
+. f . . . . f f f f f . . f . . 
+. f . . . . . . . . . . . f . . 
+. f . . . . . . . . . . . f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
